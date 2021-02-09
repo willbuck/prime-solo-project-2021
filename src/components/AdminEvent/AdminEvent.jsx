860aec0840c7963ad9382015cded8070c2147943
mnpkoerner <mnpkoerner@gmail.com>
2021-02-08 19:37:49 -0600

    datepicker capturing value from user

--- src/components/AdminEvent/AdminEvent.jsx
+++ src/components/AdminEvent/AdminEvent.jsx
@@ -1,5 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
 export default function AdminEvent() {
-    return(
-        <p>on AdminEvent</p>
+
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const [date, setDate] = useState(1111-11-11)
+
+    const checkCalendar = (checker, event) => {
+        event.preventDefault();
+        console.log(checker)
+    }
+
+
+
+    return (
+        <div>
+            <p></p>
+            <form>
+                <label htmlFor="start">Start date:</label>
+
+                <input
+                    type="date"
+                    id="start"
+                    name="trip-start"
+                    value="2018-07-22"
+                    min={new Date()}
+                    max="2040-12-31"
+                    value={date}
+                    onChange={(event)=>setDate(event.target.value)}/>
+                <button onClick={(event)=>checkCalendar(date, event)}>Submit</button>
+            </form>
+        </div>
     )
 }
