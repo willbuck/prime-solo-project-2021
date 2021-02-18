7ea00d8d974c40daced53acfa878acca75f47816
mnpkoerner <mnpkoerner@gmail.com>
2021-02-18 13:39:07 -0600

    updating landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 
 // CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
+import MountainFooter from '../Mountains/MountainFooter';
 
 function LandingPage() {
   const [heading, setHeading] = useState('Welcome');
@@ -52,7 +52,6 @@ function LandingPage() {
           </p>
         </div>
         <div className="grid-col grid-col_4">
-          <RegisterForm />
 
           <center>
             <h4>Already a Member?</h4>
@@ -62,6 +61,7 @@ function LandingPage() {
           </center>
         </div>
       </div>
+      <MountainFooter />
     </div>
   );
 }
